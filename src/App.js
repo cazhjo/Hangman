366dd1120ab3b589075c9d64e0e32ed06a0bdf47
Cazper Sihvonen <cazperhjortstam@hotmail.com>
2020-05-25 12:57:20 +0200

    Övning 8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import hang from './images/hang.jpg'
+
+class Use extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isUsed: false };
+    this.text = props.children;
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isUsed: true
+    }));
+  }
+
+  render() {
+    let btnClass = this.state.isUsed ? "inactive" : "active";
+    
+    return (
+      <button className={btnClass} onClick={this.handleClick}>
+        {this.text}
+      </button>
+    );
+    
+  }
+}
 
 function App() {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyzåäö'.split('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hänga gubbe</h1>
+      <p>Spelet går ut på att gissa ett ord</p>
+      <img src={hang} />
+
+      <div id="lines">
+        <p>_</p>
+        <p>_</p>
+        <p>_</p>
+        <p>_</p>
+        <p>_</p>
+      </div>
+      <p>Antal fel: 0</p>
+      <div id="characters">
+        {alphabet.map(x => <Use>{x}</Use>)}
+      </div>
     </div>
   );
 }
